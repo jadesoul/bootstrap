936bbf88a24d2b1dfcb2411030967ada841530c0
jadesoul <wslgb2006@gmail.com>
2012-06-28 19:20:23 +0800

    no message

--- 2-install-required-software/install.sh
+++ 2-install-required-software/install.sh
@@ -1,5 +1,10 @@
-sudo apt-get install `cat *.list`
+#apt-get
+sudo apt-get install `cat apt-get-install.list`
 
+#pip install
+sudo pip install `cat pip-install.list`
+
+#git software
 mkdir ~/git-projects
 cd ~/git-projects
 git clone https://github.com/jadesoul/libjade.git
