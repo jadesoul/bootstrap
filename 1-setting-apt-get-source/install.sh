36c7f4039bcfacbef298752a4a8575c4fc8e62ef
Shenglan Wu <wslgb2006@gmail.com>
2013-12-07 14:30:08 +0800

    fix install.sh

--- 1-setting-apt-get-source/install.sh
+++ 1-setting-apt-get-source/install.sh
@@ -2,5 +2,5 @@ SYSTEM=ubuntu-11.10
 SOURCE=163
 NAME=sources.list
 sudo mv /etc/apt/$NAME /etc/apt/$NAME.backup
-sudo cp $SYSTEM/$NAME.163 /etc/apt/$NAME
+sudo cp $SYSTEM/$NAME.$SOURCE /etc/apt/$NAME
 sudo apt-get update
